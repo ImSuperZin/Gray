bf8ac6deac43e3210acb7fb66ff7e2b074e79c52
ImSuperZin <39243594+ImSuperZin@users.noreply.github.com>
2018-09-26 18:41:49 -0300

    Update botinfo.js

--- comandos/botinfo.js
+++ comandos/botinfo.js
@@ -15,6 +15,7 @@ exports.run = (client, message, args) => {
     .addField(`:desktop: Guilds`, `**${client.guilds.size}**`, true)
     .addField(":pencil: Canais", `**${client.channels.size}**`, true)
     .addField(":busts_in_silhouette: Usuários", `**${client.users.size}**`, true)
-     .addField(":coffee: Criador", `<@272419543601643520>`, true)
+    .addField(":coffee: Criador", `<@272419543601643520>`, true)
+    .addField(":robot: Fui Criado Em", `**23/09/2018** às **21:00**`, true)
     message.channel.send(botinfo)
 }
