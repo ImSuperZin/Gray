993542481acc981a2eccfcf4801209c0b7e93662
ImSuperZin <39243594+ImSuperZin@users.noreply.github.com>
2018-09-27 12:08:41 -0300

    Update botinfo.js

--- comandos/botinfo.js
+++ comandos/botinfo.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
     .setTitle(':wave: Gray - Info')
     .setTimestamp()
     .setThumbnail(message.author.displayAvatarURl)
-    .setFooter('Meu Grupo Para Suporte [Aqui] (https://discord.gg/Gq9ZJds)')
+    .setFooter('Quer me adicione em seu grupo ---> gr!invite')
     .setDescription(`Olá, sou o Gray, sou um :boy::skin-tone-1: menino, sou um simples bot de moderação do discord.
     \n:incoming_envelope: Me invite para seu servidor, utilize **gr!invite**.`)
 
@@ -17,5 +17,6 @@ exports.run = (client, message, args) => {
     .addField(":busts_in_silhouette: Usuários", `**${client.users.size}**`, true)
     .addField(":coffee: Criador", `<@272419543601643520>`, true)
     .addField(":robot: Fui Criado Em", `**23/09/2018** às **21:00**`, true)
+    .addField(":question: Grupo de Suporte", `(Aqui) [https://discord.gg/Gq9ZJds]`, true)
     message.channel.send(botinfo)
 }
